64324ae6287f13d8230e2f332bac89eb1af5f31f
David Doyle <david@lumidion.com>
2021-06-28 07:46:04 +0200

    modifying

--- pipelines-logging/infrastructure/lambda-stack.ts
+++ pipelines-logging/infrastructure/lambda-stack.ts
@@ -6,7 +6,7 @@ export class ServiceStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props)
 
-    new NodejsFunction(this, "post-feedback-route-handler", {
+    new NodejsFunction(this, "trial", {
       entry: path.join(__dirname, "..", "index.ts"),
       timeout: Duration.seconds(30),
     })
